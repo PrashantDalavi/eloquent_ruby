887bffd9bb00c9a73586e504a40321543b3d7b6f
PrashantDalavi <p.d.dalavi@gmail.com>
2023-04-28 12:08:46 +0530

    adding notes document in rb format

---
+++ notes.rb
@@ -0,0 +1,9 @@
+=> Naming Convention
+  1. everythin in ruby is named using snake case letters
+  eg: snake_case_examle
+  using underscores and all lower letters 
+  2. classes exception to the above rule, classes use camel case
+  eg: Document/DocumentFolder
+  everyword starts with capital letter and the first word too, this is the camel case 
+
+ 
